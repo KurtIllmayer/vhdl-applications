d68c62fbb7eea1f7b51f2f1e0b9abe07f833ebdb
Kurt Illmayer <47105199+Waferstepper@users.noreply.github.com>
2019-02-19 11:57:12 +0100

    Update Sinus_n_bit_fuer_VHDL_v1.m

--- dds_functiongenerator/Sinus_n_bit_fuer_VHDL_v1.m
+++ dds_functiongenerator/Sinus_n_bit_fuer_VHDL_v1.m
@@ -1,7 +1,31 @@
-% n Bit Sinus Berechnung für VHDL
+% ---------------------------------------------------------------------------------
+% -- MIT License
+
+% -- Copyright (c) 2019 Kurt Illmayer
+
+% -- Permission is hereby granted, free of charge, to any person obtaining a copy
+% -- of this software and associated documentation files (the "Software"), to deal
+% -- in the Software without restriction, including without limitation the rights
+% -- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% -- copies of the Software, and to permit persons to whom the Software is
+% -- furnished to do so, subject to the following conditions:
+
+% -- The above copyright notice and this permission notice shall be included in all
+% -- copies or substantial portions of the Software.
+
+% -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% -- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% -- SOFTWARE.
+% ---------------------------------------------------------------------------------
+
+% n Bit Sine Calculation for VHDL
 
 disp('       ');  
-n = input('  Geben Sie die Auflösung des Signals in bit ein:          n = ');
+n = input('  Signal Resolution in bit:          n = ');
 disp('       '); 
 
 A = (2^n) - 1;
@@ -13,7 +37,7 @@ filename_vhd = ['sinusfunction_n_' num2str(n) '_bit.vhd'];
 s1 = fopen(filename_vhd,'wb');  %opens the output file
 
 fprintf(s1,'%s\n','');
-fprintf(s1,'%s\n','   -- Funktionswerte der Sinusfunktion in VHDL :');
+fprintf(s1,'%s\n','   -- Values of the sine-function in VHDL :');
 
 for i=0:A
 
