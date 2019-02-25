6d14c059ff2d4bc425c4bc2b8ea979aac6bb0818
Kurt Illmayer <47105199+Waferstepper@users.noreply.github.com>
2019-02-25 12:02:12 +0100

    Update dds_top_tb.vhd

--- dds_functiongenerator/dds_top_tb.vhd
+++ dds_functiongenerator/dds_top_tb.vhd
@@ -30,62 +30,48 @@ END dds_top_tb;
  
 ARCHITECTURE behavior OF dds_top_tb IS 
  
-    -- Component Declaration for the Unit Under Test (UUT)
- 
     COMPONENT dds_top
-    PORT(
-         clock_i : IN  std_ulogic;
-         reset_i : IN  std_ulogic;
-         clk_o : OUT  std_ulogic;
+    PORT(clock_i        : IN  std_ulogic;
+         reset_i        : IN  std_ulogic;
+         clk_o          : OUT std_ulogic;
          phase_select_i : IN  std_ulogic_vector(3 downto 0);
-         wave_select_i : IN  std_ulogic_vector(1 downto 0);
-         data_o : OUT  std_ulogic_vector(7 downto 0)
-        );
+         wave_select_i  : IN  std_ulogic_vector(1 downto 0);
+         data_o         : OUT std_ulogic_vector(7 downto 0));
     END COMPONENT;
-    
 
-   --Inputs
-   signal clock_i : std_ulogic := '0';
-   signal reset_i : std_ulogic := '0';
-   signal phase_select_i : std_ulogic_vector(3 downto 0) := (others => '0');
-   signal wave_select_i : std_ulogic_vector(1 downto 0) := (others => '0');
-
- 	--Outputs
-   signal clk_o : std_ulogic;
-   signal data_o : std_ulogic_vector(7 downto 0);
-
-   -- Clock period definitions
+   signal clock_i          : std_ulogic := '0';
+   signal reset_i          : std_ulogic := '0';
+   signal phase_select_i   : std_ulogic_vector(3 downto 0) := (others => '0');
+   signal wave_select_i    : std_ulogic_vector(1 downto 0) := (others => '0');
+   signal clk_o            : std_ulogic;
+   signal data_o           : std_ulogic_vector(7 downto 0);
    constant clock_i_period : time := 20 ns;
  
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
+
    uut: dds_top PORT MAP (
-          clock_i => clock_i,
-          reset_i => reset_i,
-          clk_o => clk_o,
+          clock_i        => clock_i,
+          reset_i        => reset_i,
+          clk_o          => clk_o,
           phase_select_i => phase_select_i,
-          wave_select_i => wave_select_i,
-          data_o => data_o
-        );
+          wave_select_i  => wave_select_i,
+          data_o         => data_o);
 
-   -- Clock process definitions
-   clock_i_process :process
-   begin
-		clock_i <= '0';
-		wait for clock_i_period/2;
-		clock_i <= '1';
-		wait for clock_i_period/2;
-   end process;
+   clock_i_p: process
+   begin 
+        clock_i <= '0';
+	wait for clock_i_period/2;
+	clock_i <= '1';
+	wait for clock_i_period/2;
+   end process clock_i_p;
 
-  reset_i <= '1' after 0 ns,
-             '0' after 10 ns;  
+   reset_i <= '1' after 0 ns,
+              '0' after 10 ns;  
 
-  phase_select_i <= "0000" after 0 ns;
+   phase_select_i <= "0000" after 0 ns;
 
-  wave_select_i <= "00" after 0 ns,
-                   "01" after 7 ms,
-                   "10" after 14 ms,
-                   "11" after 21 ms;
-
+   wave_select_i <= "00" after 0 ns,
+                    "01" after 7 ms,
+                    "10" after 14 ms,
+                    "11" after 21 ms;
 END;
